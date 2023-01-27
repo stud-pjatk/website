5360f3e6f549adc42c3c75f23ec7080394dd90a8
Julian Grajewski <jgraj@ew.ski>
2023-01-27 04:00:59 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -25,7 +25,7 @@ app.post('/wintegration/register', res => {
 app.get('/1447a325/0628/479f/a84f/89ca6010f350', res => {
 	res.onAborted(() => {});
 	res.writeHeader('Content-Type', 'text/html');
-	res.end(`<head><style>table{border-collapse:collapse}th,td{border:1px solid #000;padding:5px}</style></head><body style="display:none"><table><thead><tr><th>name</th><th>age</th><th>email</th><th>phone</th><th>other phone</th><th>year</th><th>field</th><th>diet</th><th>meds</th><tr></thead><tbody>${fs.readdirSync('wintegration').map(f => `<tr><td>${JSON.parse(fs.readFileSync(path.join('wintegration', f)).toString()).join('</td><td>')}</td></tr>`).join('')}</tbody></table></body>`);
+	res.end(`<head><meta charset="utf-8"><style>table{border-collapse:collapse}th,td{border:1px solid #000;padding:5px}</style></head><body style="display:none"><table><thead><tr><th>name</th><th>age</th><th>email</th><th>phone</th><th>other phone</th><th>year</th><th>field</th><th>diet</th><th>meds</th><tr></thead><tbody>${fs.readdirSync('wintegration').map(f => `<tr><td>${JSON.parse(fs.readFileSync(path.join('wintegration', f)).toString()).join('</td><td>')}</td></tr>`).join('')}</tbody></table></body>`);
 });
 
 app.listen('0.0.0.0', 80, token => token ? console.log(`Listening on port 80...`) : {});
