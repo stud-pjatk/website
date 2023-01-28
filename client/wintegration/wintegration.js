d09f40bc491c7e8a5a1a438768e723fbcb8073c7
Julian Grajewski <jgraj@ew.ski>
2023-01-28 08:46:22 +0100

    Update wintegration.js

--- client/wintegration/wintegration.js
+++ client/wintegration/wintegration.js
@@ -7,11 +7,9 @@ const wintegrationAutorun = () => {
 
 	form.addEventListener('submit', e => {
 		e.preventDefault();
-		const values = [...form.querySelectorAll('input, select')].map(i => i.value);
-		values.push(Date.now());
 		fetch('/wintegration/register', {
 			method: 'POST',
-			body: JSON.stringify(values),
+			body: JSON.stringify([...form.querySelectorAll('input, select')].map(i => i.value)),
 		}).then(() => {
 			document.getElementById('wintegration-register-submit').setAttribute('disabled', true);
 			document.getElementById('wintegration-register-success').style = '';
